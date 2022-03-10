bfc5ae1e96cfc8919046ca1f78662b4f71adab71
Maxime Médal <74501158+Maxime-Medal@users.noreply.github.com>
2022-03-10 01:34:30 +0100

    crud fonctionnel avec mise en mémoire cache des données

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -19,7 +19,7 @@ public class PizzaController : ControllerBase
     PizzaService.GetAll();
 
   // GET by Id action
-  [HttpGet("{id}")]
+  [HttpGet("{id}")] // Attention, pas d'espace entre les caractères
   public ActionResult<Pizza> Get(int id)
   {
     var pizza = PizzaService.Get(id);
@@ -31,9 +31,41 @@ public class PizzaController : ControllerBase
   }
 
   // POST action
+  [HttpPost]
+  public IActionResult Create(Pizza pizza)
+  {
+    PizzaService.Add(pizza);
+    return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+  }
 
   // PUT action
+  [HttpPut("{id}")] // Attention, pas d'espace entre les caractères
+  public IActionResult Update(int id, Pizza pizza)
+  {
+    if (id != pizza.Id)
+      return BadRequest();
+
+    var existingPizza = PizzaService.Get(id);
+    if (existingPizza is null)
+      return NotFound();
+
+    PizzaService.Update(pizza);
+
+    return NoContent();
+
+  }
 
   // DELETE action
+  [HttpDelete("{id}")] // Attention, pas d'espace entre les caractères
+  public IActionResult Delete(int id)
+  {
+    var pizza = PizzaService.Get(id);
+    if (pizza is null)
+      return NotFound();
+
+    PizzaService.Delete(id);
+
+    return NoContent();
+  }
 
 }
